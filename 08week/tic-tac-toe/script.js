e88ab851d7e125de4bb0ed0f65c8469287ad3a70
Sam Mangum-Bostick <spmb3434@gmail.com>
2018-12-03 18:01:50 -0600

    Finished

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,31 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic in here
-});
+$(document).ready(function(){
+  var turn = 'X';
+  var count= 0;
+  $('[data-cell]').on('click', function(){
+    if($(this).text() === ''){
+      $(this).text(turn);
+      count++;
+      checkForWin(count);
+      if(turn === 'O'){
+        turn = 'X';
+      }else{
+        turn = 'O';
+      }
+    }
+  })
+  
+  function checkForWin(tie){
+    if(tie === 9){
+      // console
+       $('#announce-winner').text(`Try again`);
+    }
+    if($('[data-cell="0"]').text() === turn &&
+      $('[data-cell="1"]').text() === turn &&
+      $('[data-cell="2"]').text() === turn){
+      $('#announce-winner').text(`${turn} wins`);
+    }
+      
+  }
+})
